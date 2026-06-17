aba2af14d802c2b60ea4cc0439b56c084f37d750
Adil Khan <adilk060505@gmail.com>
2026-06-17 16:28:54 +0530

    Create BankTransact.java

---
+++ WTL/Practical 10/BankTransact.java
@@ -0,0 +1,32 @@
+package bankexamp;
+
+import javax.ejb.Stateful;
+
+@Stateful
+
+public class BankTransact
+implements BankTransactLocal {
+
+int balance = 10000;
+
+@Override
+
+public int deposit(int amount) {
+
+balance = balance + amount;
+
+return balance;
+
+}
+
+@Override
+
+public int withdraw(int amount) {
+
+balance = balance - amount;
+
+return balance;
+
+}
+
+}
