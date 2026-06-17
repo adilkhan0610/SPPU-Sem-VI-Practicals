d9c6ccfd836be14799c150422bb8563cd2e833a1
Adil Khan <adilk060505@gmail.com>
2026-06-17 16:24:55 +0530

    Create loginbean.java

---
+++ WTL/Practical 8/loginbean.java
@@ -0,0 +1,36 @@
+package com.myapp.struts;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts.action.ActionErrors;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionMapping;
+
+public class loginbean extends ActionForm {
+
+    private String uname;
+    private String upass;
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getUpass() {
+        return upass;
+    }
+
+    public void setUpass(String upass) {
+        this.upass = upass;
+    }
+
+    public ActionErrors validate(
+            ActionMapping mapping,
+            HttpServletRequest request) {
+
+        return new ActionErrors();
+    }
+}
